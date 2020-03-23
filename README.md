e8e925f799196d12d49667fede1a0992fd450946
Ashkan Arvaneh <ashkan.arvaneh@gmail.com>
2020-03-23 09:11:08 +0000

    Update the base URL

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Vue.js-tech-test__phone-selection
+Vue.js Technical Test
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/tests/Vue.js-technical-test__phone-selection/'
+        : '/'
+}
